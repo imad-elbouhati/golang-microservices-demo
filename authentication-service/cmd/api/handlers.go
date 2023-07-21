5afb59e786bdc7279804b82a8bab55b5b06d3215
imad <elbouhatiimad@gmail.com>
2023-07-21 18:46:22 +0100

    feat: add authentication route and handler to accept JSON

---
+++ authentication-service/cmd/api/handlers.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request)  {
+	
+	var requestPayload struct {
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w,r,requestPayload)  
+
+	if err != nil {
+		app.errorJSON(w,err,http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+
+	if err != nil {
+		app.errorJSON(w,errors.New("invalid credentials"),http.StatusBadRequest)
+		return
+	}
+
+	valid, err := user.PasswordMatches(requestPayload.Password)
+
+	if err != nil ||!valid {
+		app.errorJSON(w,errors.New("invalid credentials"),http.StatusBadRequest)
+		return
+	}
+
+	payload := jsonResponse {
+		Error: false,
+		Message: fmt.Sprintf("Logged in user %s", user.Email)
+	}
+
+	app.writeJSON(w,http.StatusAccepted,payload)
+}
